c9b7bca1bd1f760cb036a62a8474bd81d2ea00fb
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-16 17:51:10 -0700

    added moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,34 @@ temperatureEl = document.getElementById("temperature");
 humidityEl = document.getElementById("humidity");
 windEl = document.getElementById("wind");
 uvindexEl = document.getElementById("uvindex");
-forecastBody = document.getElementById("5forecast");
+forecastBodyEl = document.getElementById("5forecast");
 searchedCityEl = document.getElementById("currentcity");
+upcomingDay1El = document.getElementById("upcomingday1");
+temperature1El = document.getElementById("temperature1");
+humidity1El = document.getElementById("humidity1");
+wind1El = document.getElementById("wind1");
+upcomingDay2El = document.getElementById("upcomingday2");
+temperature2El = document.getElementById("temperature2");
+humidity2El = document.getElementById("humidity2");
+wind2El = document.getElementById("wind2");
+upcomingDay3El = document.getElementById("upcomingday3");
+temperature3El = document.getElementById("temperature3");
+humidity3El = document.getElementById("humidity3");
+wind3El = document.getElementById("wind3");
+upcomingDay4El = document.getElementById("upcomingday4");
+temperature4El = document.getElementById("temperature4");
+humidity4El = document.getElementById("humidity4");
+wind4El = document.getElementById("wind4");
+upcomingDay5El = document.getElementById("upcomingday5");
+temperature5El = document.getElementById("temperature5");
+humidity5El = document.getElementById("humidity5");
+wind5El = document.getElementById("wind5");
+todayEl = document.getElementById("today");
+day1El = document.getElementById("day1");
+day2El = document.getElementById("day2");
+day3El = document.getElementById("day3");
+day4El = document.getElementById("day4");
+day5El = document.getElementById("day5");
 var searchedCities = [];
 
 
@@ -20,7 +46,8 @@ function weather(cityname){
         if(response.ok){
             response.json().then(function(response) {
                 // console.log(response);
-                cityNameEl.textContent = searchedCityEl.value;
+                cityNameEl.textContent = searchedCityEl.value ;
+                todayEl.textContent = moment();
                 temperatureEl.textContent = "Temperature: " + tempConversion(response.main.temp) + "F";
                 humidityEl.textContent = "Humidity: " +  response.main.humidity + "%";
                 windEl.textContent = "Wind: " + response.wind.speed + "MPH";
@@ -33,6 +60,33 @@ function weather(cityname){
                         // console.log(response);
                         uvindexEl.textContent = "UV index: " + response.current.uvi;
                     })
+                })
+                var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial" + "&appid=" + apiKey;
+                fetch(forecastUrl).then(function(response){
+                        response.json().then(function(response){
+                            console.log(response);
+                            upcomingDay1El.textContent =  (moment().add(1, "days"));
+                            temperature1El.textContent =  response.list[0].main.temp;
+                            humidity1El.textContent = response.list[0].main.humidity;
+                            wind1El.textContent =  response.list[0].wind.speed;
+                            upcomingDay2El.textContent =  (moment().add(2, "days"));
+                            temperature2El.textContent =  response.list[8].main.temp;
+                            humidity2El.textContent = response.list[8].main.humidity;
+                            wind2El.textContent =  response.list[8].wind.speed;
+                            upcomingDay3El.textContent =  (moment().add(3, "days"));
+                            temperature3El.textContent =  response.list[16].main.temp;
+                            humidity3El.textContent = response.list[16].main.humidity;
+                            wind3El.textContent =  response.list[16].wind.speed;
+                            upcomingDay4El.textContent =  (moment().add(4, "days"));
+                            temperature4El.textContent =  response.list[24].main.temp;
+                            humidity4El.textContent = response.list[24].main.humidity;
+                            wind4El.textContent =  response.list[24].wind.speed;
+                            upcomingDay5El.textContent =  (moment().add(5, "days"));
+                            temperature5El.textContent =  response.list[32].main.temp;
+                            humidity5El.textContent = response.list[32].main.humidity;
+                            wind5El.textContent =  response.list[32].wind.speed;
+                        })
+                    
                 })
               })
             } else {
