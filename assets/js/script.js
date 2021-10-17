abaf18c102ce75a00718cbfbc5e0578ceed19f86
tarekyou <89763835+tarekyou@users.noreply.github.com>
2021-10-16 20:13:45 -0700

    added path to icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,6 +33,7 @@ day2El = document.getElementById("day2");
 day3El = document.getElementById("day3");
 day4El = document.getElementById("day4");
 day5El = document.getElementById("day5");
+nameDivEl = document.getElementById("namediv");
 var searchedCities = [];
 
 
@@ -45,7 +46,10 @@ function weather(cityname){
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(response) {
-                // console.log(response);
+                console.log(response);
+                var weatherIconEl = document.createElement("img");
+                weatherIconEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+                nameDivEl.append(weatherIconEl);
                 cityNameEl.textContent = searchedCityEl.value ;
                 todayEl.textContent = moment();
                 temperatureEl.textContent = "Temperature: " + tempConversion(response.main.temp) + "F";
@@ -64,23 +68,38 @@ function weather(cityname){
                 var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityname + "&units=imperial" + "&appid=" + apiKey;
                 fetch(forecastUrl).then(function(response){
                         response.json().then(function(response){
-                            console.log(response);
+                            // console.log(response);
+                            var weatherIcon1El = document.createElement("img");
+                            weatherIcon1El.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png");
+                            day1El.append(weatherIcon1El);
                             upcomingDay1El.textContent =  (moment().add(1, "days"));
                             temperature1El.textContent =  response.list[0].main.temp;
                             humidity1El.textContent = response.list[0].main.humidity;
                             wind1El.textContent =  response.list[0].wind.speed;
+                            var weatherIcon2El = document.createElement("img");
+                            weatherIcon2El.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png");
+                            day2El.append(weatherIcon2El);
                             upcomingDay2El.textContent =  (moment().add(2, "days"));
                             temperature2El.textContent =  response.list[8].main.temp;
                             humidity2El.textContent = response.list[8].main.humidity;
                             wind2El.textContent =  response.list[8].wind.speed;
+                            var weatherIcon3El = document.createElement("img");
+                            weatherIcon3El.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png");
+                            day3El.append(weatherIcon3El);
                             upcomingDay3El.textContent =  (moment().add(3, "days"));
                             temperature3El.textContent =  response.list[16].main.temp;
                             humidity3El.textContent = response.list[16].main.humidity;
                             wind3El.textContent =  response.list[16].wind.speed;
+                            var weatherIcon4El = document.createElement("img");
+                            weatherIcon4El.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[24].weather[0].icon + "@2x.png");
+                            day4El.append(weatherIcon4El);
                             upcomingDay4El.textContent =  (moment().add(4, "days"));
                             temperature4El.textContent =  response.list[24].main.temp;
                             humidity4El.textContent = response.list[24].main.humidity;
                             wind4El.textContent =  response.list[24].wind.speed;
+                            var weatherIcon5El = document.createElement("img");
+                            weatherIcon5El.setAttribute("src", "https://openweathermap.org/img/wn/" + response.list[32].weather[0].icon + "@2x.png");
+                            day5El.append(weatherIcon5El);
                             upcomingDay5El.textContent =  (moment().add(5, "days"));
                             temperature5El.textContent =  response.list[32].main.temp;
                             humidity5El.textContent = response.list[32].main.humidity;
